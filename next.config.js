eef1bd03f59bb04d750d67bb1b92d07cb2131d95
Skrub <44077242+triplebproductionzz@users.noreply.github.com>
2024-06-07 14:13:25 -0600

    changed package.json, still getting error with emails

--- next.config.js
+++ next.config.js
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: "http",
-				hostname: "localhost",
-			}
-		],
-	},
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+      {
+        protocol: "https",
+        hostname: "digitalhippo-production.up.railway.app",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
